f5ce2413ba8f86833c7fcb4eb10ea6ff33c18f43
Mrazadomas <117606034+Mrazadomas@users.noreply.github.com>
2025-09-07 18:12:51 -0500

    done

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -1,11 +1,26 @@
 package org.example;
+import java.util.Scanner;
 
 public class App {
   public int getInput() {
-    // Implement your function here
+    int input;
+    Scanner scanner = new Scanner(System.in);
+    do {
+        System.out.print("\nPlease enter an integer value: ");
+        input = scanner.nextInt();
+        if ((input < 0) || (input > 10)) {
+            System.out.println("\nPlease enter a valid integer value.");
+        }
+    }
+    while ((input < 0) || (input > 10)); 
+    scanner.close();
+    return input;
   }
+  
 
   public static void main(String[] args) {
-    // Test your function here in a driver program
+    App app = new App();
+    int value = app.getInput();
+    System.out.println("\nThe value chosen by the user is: " + value);
   }
 }
